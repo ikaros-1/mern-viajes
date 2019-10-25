00893a63990b69fa55d099ec87c7fc5e766aefe5
ikaros-1 <ian_9999@hotmail.com>
2019-10-25 02:22:52 -0300

    inicio task 5

---
+++ src/components/Home-two/Menu.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export default class Menu extends Component{
+
+    render(){
+        return(
+            <div>
+                <img src="/image/list.png"></img>
+            </div>
+        );
+    }
+}
